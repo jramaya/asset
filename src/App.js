1a724ef52fe3d027d534e8ec85689fa994d29969
jramaya <jr.amaya@yandex.com>
2018-04-27 23:18:52 -0600

    App naming update

--- src/App.js
+++ src/App.js
@@ -236,7 +236,7 @@ class App extends Component {
         <header className="mdc-toolbar mdc-toolbar--fixed">
           <div className="mdc-toolbar__row">
             <section className="mdc-toolbar__section mdc-toolbar__section--align-start">
-              <span className="mdc-toolbar__title">Expenses</span>
+              <span className="mdc-toolbar__title">Ingresos</span>
             </section>
             <section
               className="mdc-toolbar__section mdc-toolbar__section--align-end"
